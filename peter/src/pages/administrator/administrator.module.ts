867bef2ab8db9c1faf67cafc1eb9388c7758257c
Lamancha6367 <jungwook8580@gmail.com>
2018-03-02 12:05:33 +0900

    march 2nd update!

--- peter/src/pages/administrator/administrator.module.ts
+++ peter/src/pages/administrator/administrator.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AdministratorPage } from './administrator';
+
 @NgModule({
   declarations: [
     AdministratorPage,
@@ -9,4 +10,5 @@ import { AdministratorPage } from './administrator';
     IonicPageModule.forChild(AdministratorPage),
   ],
 })
+
 export class AdministratorPageModule {}
--- peter/src/pages/administrator/administrator.ts
+++ peter/src/pages/administrator/administrator.ts
@@ -9,29 +9,26 @@ import {DuckMainPage} from '../duck-main/duck-main';
 import {GarbagePage} from '../garbage/garbage';
 
 @IonicPage()
+
 @Component({
   selector: 'page-administrator',
   templateUrl: 'administrator.html',
 })
+
 export class AdministratorPage {
   currentPage='GarbagePage';
 
   myDate:string;
 
   constructor(private app:App, public navCtrl: NavController, public navParams: NavParams) {
-
-   
     var d = new Date();
     var mm = d.getMonth() < 9 ? "0" +(d.getMonth() + 1) : (d.getMonth() + 1); //getMonth()
     var dd = d.getDate() < 10 ? "0" + d.getDate() : d.getDate();
     var hh = d.getHours() < 10? "0" + d.getHours() : d.getHours();
     var min = d.getMinutes() < 10? "0" + d.getMinutes():d.getMinutes();
     var dString=d.getFullYear()+'-'+(mm)+'-'+dd+'T'+hh+":"+min+moment().format("Z");
-    
     //var dString=d.toISOString(); //UTC time
     this.myDate=dString; 
-
-
   }
 
   ionViewDidLoad() {
@@ -49,6 +46,7 @@ export class AdministratorPage {
     this.app.getRootNavs()[0].setRoot(GarbagePage);
     setTimeout(() => { this.currentPage='GarbagePage';}, 500);
     }
+    
   checkInput(){
     console.log(this.myDate);
     var date=new Date(this.myDate);
