541ee19876cbe422c6e09409f8f3798333a21adb
jjs <jjs@jjss-Mac.local>
2018-03-06 13:44:58 +0900

    order_Input_Modify

--- jason/src/pages/home/home.ts
+++ jason/src/pages/home/home.ts
@@ -7,37 +7,57 @@ import * as moment from 'moment';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
 
+  //member variables//
   orderItems;
-  myDate:string;
+  myDate: string;
   myDateYesterday: string;
   myDateTomorrow: string;
- 
+
+  deliverTime: string;
+  recipientAddress: string;
+  buyerName: string;
+  recipientName: string;
+  recipientPhoneNumber: string;
+  buyerPhoneNumber: string;
+  dduckName: string;
+  dduckAmount: string;
+  dduckUnit: string;
+  memo: string;
+  price: string;
+  paymentOptions: string;
+  paymentPlan: string;
+
+  //ngFor variables//
+  orderList=[];
+  dduckAddedList=[];
+
+
+
   constructor(public navCtrl: NavController) {
-      this.initializeItems();
-      var d = new Date();
-      var mm = d.getMonth() < 9? "0" + (d.getMonth() + 1) : (d.getMonth() +1);
-      var dd = d.getDate() <10? "0" + d.getDate() : d.getDate();
-      var dddd = d.getDay();
-      var ddYesterday = d.getDate()-1 <10? "0" + d.getDate() : d.getDate();
-      var ddTomorrow = d.getDate()+1 <10? "0" + d.getDate() : d.getDate();
-      var hh = d.getHours() <10? "0" + d.getHours() : d.getHours();
-      var min = d.getMinutes() <10? "0"+d.getMinutes() : d.getMinutes();
-      
-      var dString = d.getFullYear()+ '-' + (mm) + '-' + (dd) + 'T' + hh + ":" + min+ moment().format("Z") + '-' +(dddd);
-      var dStringYesterday = d.getFullYear()+ '-' + (mm) + '-' + (ddYesterday) + 'T' + hh + ":" + min+ moment().format("Z");
-      var dStringTomorrow = d.getFullYear()+ '-' + (mm) + '-' + (ddTomorrow) + 'T' + hh + ":" + min+ moment().format("Z");
-  
-      this.myDate = dString;
-      
-      this.myDateYesterday = dStringYesterday;
-      this.myDateTomorrow = dStringTomorrow;
+    this.initializeItems();
+    var d = new Date();
+    var mm = d.getMonth() < 9 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1);
+    var dd = d.getDate() < 10 ? "0" + d.getDate() : d.getDate();
+    var dddd = d.getDay();
+    var ddYesterday = d.getDate() - 1 < 10 ? "0" + d.getDate() : d.getDate();
+    var ddTomorrow = d.getDate() + 1 < 10 ? "0" + d.getDate() : d.getDate();
+    var hh = d.getHours() < 10 ? "0" + d.getHours() : d.getHours();
+    var min = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
+
+    var dString = d.getFullYear() + '-' + (mm) + '-' + (dd) + 'T' + hh + ":" + min + moment().format("Z") + '-' + (dddd);
+    var dStringYesterday = d.getFullYear() + '-' + (mm) + '-' + (ddYesterday) + 'T' + hh + ":" + min + moment().format("Z");
+    var dStringTomorrow = d.getFullYear() + '-' + (mm) + '-' + (ddTomorrow) + 'T' + hh + ":" + min + moment().format("Z");
+
+    this.myDate = dString;
+
+    this.myDateYesterday = dStringYesterday;
+    this.myDateTomorrow = dStringTomorrow;
 
   }
 
 
-  initializeItems(){
+  initializeItems() {
     this.orderItems = [
       'Seoul',
       'Hong Kong',
@@ -49,7 +69,7 @@ export class HomePage {
     ];
   }
 
-  getOrderItems(ev){
+  getOrderItems(ev) {
     // Reset items back to all of the items;
     this.initializeItems();
 
@@ -57,10 +77,8 @@ export class HomePage {
     var val = ev.target.value;
 
     //if the value is an empty string don't filter the items
-    if(val && val.trim() != '')
-    {
-      this.orderItems = this.orderItems.filter((item) =>
-      {
+    if (val && val.trim() != '') {
+      this.orderItems = this.orderItems.filter((item) => {
         return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
     }
@@ -71,53 +89,53 @@ export class HomePage {
     console.log('ionViewDidLoad Page3Page');
   }
 
-  orderButtonColor="#508AE4";
-  deliverButtonColor="#e1e1e1";
-  produceButtonColor="#e1e1e1";
+  orderButtonColor = "#508AE4";
+  deliverButtonColor = "#e1e1e1";
+  produceButtonColor = "#e1e1e1";
 
-  orderButtonFlag:boolean=false;
-  deliverButtonFlag:boolean=true;
-  produceButtonFlag:boolean=true;
-  
-  orderButton(){
-    this.orderButtonColor="#508AE4";
-    this.deliverButtonColor="#e1e1e1";
-    this.produceButtonColor="#e1e1e1";
+  orderButtonFlag: boolean = false;
+  deliverButtonFlag: boolean = true;
+  produceButtonFlag: boolean = true;
 
-    this.deliverButtonFlag=true;
-    this.produceButtonFlag=true;
-    this.orderButtonFlag=false;
+  orderButton() {
+    this.orderButtonColor = "#508AE4";
+    this.deliverButtonColor = "#e1e1e1";
+    this.produceButtonColor = "#e1e1e1";
+
+    this.deliverButtonFlag = true;
+    this.produceButtonFlag = true;
+    this.orderButtonFlag = false;
   }
 
-  deliverButton(){
-    this.deliverButtonColor="#508AE4";
-    this.orderButtonColor="#e1e1e1";
-    this.produceButtonColor="#e1e1e1";
+  deliverButton() {
+    this.deliverButtonColor = "#508AE4";
+    this.orderButtonColor = "#e1e1e1";
+    this.produceButtonColor = "#e1e1e1";
 
-    this.orderButtonFlag=true;
-    this.produceButtonFlag=true;
-    this.deliverButtonFlag=false;
+    this.orderButtonFlag = true;
+    this.produceButtonFlag = true;
+    this.deliverButtonFlag = false;
 
-    
+
   }
 
-  produceButton(){
-    this.produceButtonColor="#508AE4";
-    this.orderButtonColor="#e1e1e1";
-    this.deliverButtonColor="#e1e1e1";
+  produceButton() {
+    this.produceButtonColor = "#508AE4";
+    this.orderButtonColor = "#e1e1e1";
+    this.deliverButtonColor = "#e1e1e1";
 
-    this.orderButtonFlag=true;
-    this.deliverButtonFlag=true;
-    this.produceButtonFlag=false;
+    this.orderButtonFlag = true;
+    this.deliverButtonFlag = true;
+    this.produceButtonFlag = false;
   }
 
-  
 
-  goYesterday(){
+
+  goYesterday() {
     var dateYesterday = new Date(this.myDateYesterday);
   }
 
-  goTomorrow(){
+  goTomorrow() {
     var dateTomorrow = new Date(this.myDateTomorrow);
   }
 
@@ -126,5 +144,25 @@ export class HomePage {
     var date=new Date(this.myDate);
     console.log("month: "+date.getMonth()+ "  date: "+date.getDate()+"  hour: "+date.getHours())
   }*/
+
+
+  save(){
+    let order={deliverTime: this.deliverTime, recipientAddress: this.recipientAddress, recipientName: this.recipientName
+    , recipientPhoneNumber: this.recipientPhoneNumber, buyerName: this.buyerName, buyerPhoneNumber: this.buyerPhoneNumber
+    , dduckAddedList: this.dduckAddedList, memo: this.memo, price: this.price, paymentOptions: this.paymentOptions, paymentPlan: this.paymentPlan}
+      
+    this.orderList.push(order);
+    console.log(JSON.stringify(this.orderList));
+    this.dduckAddedList=[];
+  }
+
+  dduckAdd(){
+    let dduckAdded={dduckName: this.dduckName, dduckAmount:this.dduckAmount, dduckUnit: this.dduckUnit}
+    this.dduckAddedList.push(dduckAdded);
+    this.dduckName=undefined;
+    this.dduckAmount=undefined;
+    this.dduckUnit=undefined;
+  }  
+
 
 }
